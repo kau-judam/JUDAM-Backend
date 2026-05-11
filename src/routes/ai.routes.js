c6e2189f7c519fc32f537df123bb2d5066b1e58c
유승빈 <devaixq@gmail.com>
2026-05-11 16:49:27 +0900

    Revert "Merge pull request #34 from kau-judam/feature/infra-env-from-aws"
    This reverts commit 9d56c065df8c51101191127c295f9fa8eed0c8a3, reversing
    changes made to e621a142dbdf21ca0fd34d4ff0f9556d55835f4b.

--- src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAiHealth } = require('../controllers/ai.controller');
-
-const router = express.Router();
-
-router.get('/health', getAiHealth);
-
-module.exports = router;
