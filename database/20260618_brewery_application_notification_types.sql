7e66e8aea5154f4b59f1792328bca002b2531c51
유승빈 <devaixq@gmail.com>
2026-06-18 01:41:23 +0900

    feat: 양조장 인증 상태 변경 알림 추가

---
+++ database/20260618_brewery_application_notification_types.sql
@@ -0,0 +1,18 @@
+ALTER TABLE brewery_dashboard_notifications
+  DROP CONSTRAINT IF EXISTS chk_brewery_dashboard_notifications_type;
+
+ALTER TABLE brewery_dashboard_notifications
+  ADD CONSTRAINT chk_brewery_dashboard_notifications_type
+  CHECK (
+    type IN (
+      'FUNDING_CREATED',
+      'FUNDING_PROGRESS',
+      'FUNDING_ENDED',
+      'FUNDING_SUCCESS',
+      'SETTLEMENT_COMPLETED',
+      'RECIPE_POPULAR',
+      'BREWERY_APPLICATION_SUBMITTED',
+      'BREWERY_APPLICATION_APPROVED',
+      'BREWERY_APPLICATION_REJECTED'
+    )
+  );
